961e134bd77cc97a2eb0872e93556ff94b452677
Erwin Andrés <erwinlarios65@gmail.com>
2017-01-11 21:44:55 -0500

    Minificar html con gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
 var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
+
+gulp.task('html', function() {
+  return gulp.src('src/*.html')
+    .pipe(htmlmin({ colapsewhitespace: true }))
+    .pipe(gulp.dest('dist/'));
+});
 
 gulp.task('styles', function() {
   return gulp.src('src/styles/*.css')
@@ -18,4 +25,4 @@ gulp.task('clean', function(){
     return del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img']);
 });
 
-gulp.task('default', ['styles', 'scripts']);
+gulp.task('default', ['html', 'styles', 'scripts']);
