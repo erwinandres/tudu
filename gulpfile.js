eb4f165b1a25edbf92b3112f74a97066cb4e8416
Erwin Andrés <erwinlarios65@gmail.com>
2017-01-11 22:16:32 -0500

    Nuevas tareas gulp.
    	- Images: optimiza imágenes
    	- Clean: Elimina del 'dist' archivos y carpetas eliminadas del 'src'.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
 var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
+var cache = require('gulp-cache');
+var imagemin = require('gulp-imagemin');
+var del = require('del');
 
 gulp.task('html', function() {
   return gulp.src('src/*.html')
@@ -21,8 +24,16 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('dist/assets/js'));
 });
 
+gulp.task('images', function(){
+    return gulp.src('src/images/*')
+        .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+        .pipe(gulp.dest('dist/assets/img'))
+});
+
 gulp.task('clean', function(){
     return del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img']);
 });
 
-gulp.task('default', ['html', 'styles', 'scripts']);
+gulp.task('default', ['clean'], function() {
+  gulp.start('html', 'styles', 'scripts', 'images');
+});
