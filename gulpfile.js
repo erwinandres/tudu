7d9fc17cd0c644284ee14391f239845bdd842010
Erwin Andrés <erwinlarios65@gmail.com>
2017-02-06 15:49:35 -0500

    Remove the 'assets' folder from 'dist'.

--- gulpfile.js
+++ gulpfile.js
@@ -19,11 +19,11 @@ gulp.task('useref', function() {
 gulp.task('images', function(){
     return gulp.src('src/img/*')
         .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
-        .pipe(gulp.dest('dist/assets/img'))
+        .pipe(gulp.dest('dist/img'))
 });
 
 gulp.task('clean', function(){
-    return del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img']);
+    return del(['dist/css', 'dist/js', 'dist/img']);
 });
 
 gulp.task('default', ['clean'], function() {
