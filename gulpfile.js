48b81256de6910b6bc39026c0054621e549a550c
Erwin Andrés <erwinlarios65@gmail.com>
2017-02-06 16:04:23 -0500

    Gulp task to copy other statics assets.

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,14 @@ var imagemin = require('gulp-imagemin');
 var useref = require('gulp-useref');
 var del = require('del');
 
+var assets = [
+  'src/**',
+  '!src/index.html',
+  '!src/styles{,/**}',
+  '!src/img{,/**}',
+  '!src/scripts{,/**}'
+];
+
 gulp.task('useref', function() {
   return gulp.src('src/*.html')
     .pipe(useref())
@@ -22,10 +30,15 @@ gulp.task('images', function(){
         .pipe(gulp.dest('dist/img'))
 });
 
+gulp.task('copy', function() {
+  return gulp.src(assets)
+    .pipe(gulp.dest('dist'))
+})
+
 gulp.task('clean', function(){
-    return del(['dist/css', 'dist/js', 'dist/img']);
+    return del('dist/**/*');
 });
 
 gulp.task('default', ['clean'], function() {
-  gulp.start('useref', 'images');
+  gulp.start('useref', 'images', 'copy');
 });
