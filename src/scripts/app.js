42b62f46ec1a036b66677b8650a31ec4c1a03b7f
Erwin Andrés <erwinlarios65@gmail.com>
2017-02-13 02:54:37 -0500

    Add a toast engine.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -421,6 +421,50 @@ function saveList(data, listId) {
   loadHome();
 }
 
+function Toast(container) {
+  var container = container;
+  var className = 'toast';
+
+  this.simple = function(text) {
+    var toast = createToast(text);
+    container.appendChild(toast);
+
+    deleteToast(toast, 3000);
+  }
+
+  this.action = function(text, buttonText, buttonAction) {
+    var toast = createToast(text);
+
+    var button = createEl('button', buttonText, 'toast-action');
+    button.addEventListener('click', buttonAction);
+
+    toast.appendChild(button);
+
+    container.appendChild(toast);
+
+    deleteToast(toast, 3000);
+  }
+
+  function createToast(text) {
+    var toast = createEl('div', '', className);
+    var toastText = createEl('span', text, 'toast-text');
+
+    toast.appendChild(toastText);
+
+    return toast;
+  }
+
+  function deleteToast (toast, delay) {
+    setTimeout(function() {
+      container.removeChild(toast);
+    }, delay);
+  }
+}
+
+//Init toast
+var toastContainer = document.getElementById('toast-container');
+var toast = new Toast(toastContainer);
+
 document.body.onload = function() {
   setDB();
   loadHome();
@@ -440,3 +484,4 @@ if ('serviceWorker' in navigator) {
     console.log('Service worker ready');
   });
 }
+ 
