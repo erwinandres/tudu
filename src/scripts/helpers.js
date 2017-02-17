9feef58951ef80d4de6cb312fb57fec68ce03d63
Erwin Andrés <erwinlarios65@gmail.com>
2017-02-17 16:59:07 -0500

    New colors.

--- src/scripts/helpers.js
+++ src/scripts/helpers.js
@@ -54,13 +54,23 @@ function getRandomInt(min, max) {
 function getRandomColor() {
   var colors = [
     '#FFCDD2',
+    '#F8BBD0',
     '#E1BEE7',
     '#D1C4E9',
     '#C5CAE9',
     '#BBDEFB',
+    '#B3E5FC',
+    '#B2EBF2',
     '#B2DFDB',
+    '#C8E6C9',
+    '#DCEDC8',
     '#F0F4C3',
-    '#FFECB3'
+    '#FFF9C4',
+    '#FFECB3',
+    '#FFE0B2',
+    '#FFCCBC',
+    '#D7CCC8'
+
   ];
 
   var index = getRandomInt(0, colors.length-1);
